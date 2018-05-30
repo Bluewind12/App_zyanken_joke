5ad56126a632ea9e3ef7ef09a3bddea009d66f20
Bluewind12 <zero18030@gmail.com>
2018-05-30 13:57:59 +0900

    firstChoiceScreen_Create

--- app/src/main/java/com/example/momonyan/app_zyanken_joke/MainActivity.kt
+++ app/src/main/java/com/example/momonyan/app_zyanken_joke/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.momonyan.app_zyanken_joke
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var startButton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val start = findViewById(R.id.startB) as Button
-        start.setOnClickListener {
+        startButton = findViewById(R.id.startB) as Button
+        startButton.setOnClickListener {
             Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
+            val intant = Intent(this,firstChoiceScreen::class.java)
+            startActivity(intant)
         }
     }
 }
---
+++ app/src/main/java/com/example/momonyan/app_zyanken_joke/firstChoiceScreen.kt
@@ -0,0 +1,11 @@
+package com.example.momonyan.app_zyanken_joke
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class firstChoiceScreen :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.janken_first)
+    }
+}
