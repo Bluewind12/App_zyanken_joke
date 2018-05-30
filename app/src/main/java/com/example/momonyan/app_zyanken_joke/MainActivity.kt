1b52d37e635e49e467d9880710e39d858f9be4fe
Bluewind12 <zero18030@gmail.com>
2018-05-30 09:06:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/momonyan/app_zyanken_joke/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.momonyan.app_zyanken_joke
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
