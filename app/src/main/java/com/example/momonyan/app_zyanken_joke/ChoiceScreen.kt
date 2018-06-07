9349ba3b66fbfb6d628a835dd080f185dec9bc31
Bluewind12 <zero18030@gmail.com>
2018-06-07 22:37:47 +0900

    Correction

--- app/src/main/java/com/example/momonyan/app_zyanken_joke/ChoiceScreen.kt
+++ app/src/main/java/com/example/momonyan/app_zyanken_joke/ChoiceScreen.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageButton
 
 class ChoiceScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.janken_first)
 
-        val rockButton = findViewById<Button>(R.id.gu_button)
-        val scissorsButton = findViewById<Button>(R.id.choki_button)
-        val paperButton = findViewById<Button>(R.id.pa_button)
+        val rockButton = findViewById<ImageButton>(R.id.gu_button)
+        val scissorsButton = findViewById<ImageButton>(R.id.choki_button)
+        val paperButton = findViewById<ImageButton>(R.id.pa_button)
         val winner = intent.getIntExtra("winner", 0)
 
         val nextIntent = Intent(this, QuestionScreen::class.java)
